a292e7856962836ef9aa5534ae961d6f6813d240
Garvit Banga <bangaboy181@gmail.com>
2025-04-04 20:08:02 -0400

    routes

---
+++ apps/http-backend/src/authmiddleware.ts
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import {NextFunction, Request, Response} from "express";
+import { JWT_SECRET } from "@repo/backend-common/config";
+export function authmiddleware(req:Request,res:Response,next:NextFunction){
+    const token=req.headers.authorization??"";
+    if(token){
+        const decoded=jwt.verify(token,JWT_SECRET);
+        
+        if(decoded){
+            // @ts-ignore TODO: fix this
+            req.userid=decoded.userid;
+            next();
+        } 
+        else{
+            res.status(401).json({message:"Invalid token"});
+        }
+    }
+
+
+
+
+}
--- apps/ws-backend/src/index.tsx
+++ apps/ws-backend/src/index.tsx
@@ -1,9 +1,33 @@
 import {WebSocketServer} from "ws";
 import axios from "axios";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", (ws) => {
+wss.on("connection", (ws,request) => {
+
+
+  const url=request.url;
+  if(!url){
+      ws.send("Invalid request");
+      return;
+  }
+  const querparams=new URLSearchParams(url.split("?")[1]);
+  const token=querparams.get("token")||"";
+  const decoded=jwt.verify(token,JWT_SECRET);
+  if(!decoded||!(decoded as JwtPayload).userid){
+      // ws.send("Invalid token");
+      ws.close();
+      return;
+  }
+  ws.send("Connected");
+
+
+
+
+
+
   ws.on("message", (message) => {
     console.log("received: %s", message);
     ws.send("Hello World!");
