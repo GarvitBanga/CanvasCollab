8e584b8a7d32eb3a87f820b0d2e07ebfdea7a5b8
Garvit Banga <bangaboy181@gmail.com>
2025-04-08 01:16:50 -0400

    package changes

--- apps/canvascollab-frontend/config.ts
+++ apps/canvascollab-frontend/config.ts
@@ -1,2 +1,2 @@
-export const HTTP_BACKEND_URL="http://localhost:3001";
-export const WS_URL="ws://localhost:8080";
+export const HTTP_BACKEND_URL="http://192.168.1.162:3001";
+export const WS_URL="ws://192.168.1.162:8080";
