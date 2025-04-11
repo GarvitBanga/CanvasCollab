bfcd6f92b8ea4041e8670c603ddab1a5c2f6b04b
Garvit Banga <bangaboy181@gmail.com>
2025-04-11 12:02:57 -0400

    canvas page error because of params

--- apps/canvascollab-frontend/app/canvas/[roomId]/page.tsx
+++ apps/canvascollab-frontend/app/canvas/[roomId]/page.tsx
@@ -3,14 +3,17 @@
 // import { use, useEffect, useRef } from "react";
 // import dynamic from "next/dynamic"
 import ClientAuthCheck from "@/components/ClientAuthCheck";
-
+import type { Metadata, ResolvingMetadata } from 'next'
 // You can define your own props explicitly like this:
-type CanvasPageProps = {
-  params: {
-    roomId: string;
-  };
-};
+type Props = {
+    params: Promise<{ roomId: string }>
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+  }
+   
 
-export default function CanvasPage({ params }: CanvasPageProps) {
-  return <ClientAuthCheck roomId={params.roomId} />;
+export default async function CanvasPage( { params, searchParams }: Props,
+    parent: ResolvingMetadata
+  ) {
+    const { roomId } = await params;
+  return <ClientAuthCheck roomId={roomId} />;
 }
